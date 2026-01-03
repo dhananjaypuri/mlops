4ebd2fbcb0213bed8693ff806d4e1070a0f26b4d
dhananjaypuri <50763591+dhananjaypuri@users.noreply.github.com>
2026-01-03 18:19:55 +0530

    Update linear_reg.py

--- Linear-Regression/linear_reg.py
+++ Linear-Regression/linear_reg.py
@@ -9,8 +9,6 @@ df
 
 ## Detecting NaN - missing data
 df.isna().sum()
-print(df['hours_studied'].mean());
-print(df['marks_obtained'].mean());
 
 ## Replacing NaN - missing data
 
@@ -27,7 +25,8 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_st
 
 model.fit(X_train, y_train);
 
-marks_predicted = model.predict([[6]]);
+input_hours = float(input("Enter the hours you study : "));
+marks_predicted = model.predict([[ input_hours ]]);
 
-print(float(marks_predicted));
+print(f"Based on the input hours studied you will score : {float(marks_predicted)} marks");
 
